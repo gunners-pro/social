04c1a07a36785c4f04e9bd50c13c81801fe88667
Fabricyo Barreto <bass.barreto@gmail.com>
2024-10-22 14:40:13 -0300

    feat: Implementation of firebase auth repository

--- lib/features/auth/data/firebase_auth_repo.dart
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -1,29 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:social/features/auth/domain/entities/app_user.dart';
 import 'package:social/features/auth/domain/repository/auth_repo.dart';
 
 class FirebaseAuthRepository implements AuthRepository {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
   @override
-  Future<AppUser?> loginWithEmailPassword(String email, String password) {
-    // TODO: implement loginWithEmailPassword
-    throw UnimplementedError();
+  Future<AppUser?> loginWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        email: email,
+        name: '',
+      );
+
+      return user;
+    } catch (e) {
+      throw Exception('Login failed: $e');
+    }
   }
 
   @override
   Future<AppUser?> registerWithEmailPassword(
-      String name, String email, String password) {
-    // TODO: implement registerWithEmailPassword
-    throw UnimplementedError();
+      String name, String email, String password) async {
+    try {
+      UserCredential userCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        email: email,
+        name: name,
+      );
+
+      return user;
+    } catch (e) {
+      throw Exception('Login failed: $e');
+    }
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> logout() async {
+    firebaseAuth.signOut();
   }
 
   @override
-  Future<AppUser?> getCurrentUser() {
-    // TODO: implement getCurrentUser
-    throw UnimplementedError();
+  Future<AppUser?> getCurrentUser() async {
+    final firebaseUser = firebaseAuth.currentUser;
+
+    if (firebaseUser == null) {
+      return null;
+    }
+
+    return AppUser(
+      uid: firebaseUser.uid,
+      email: firebaseUser.email!,
+      name: '',
+    );
   }
 }
